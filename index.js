1855412b97b86298ef3ec01f934cbfa28cb6358d
bigtoe <xxxlata@naver.com>
2021-12-20 23:34:52 +0900

    clone

--- index.js
+++ index.js
@@ -3,18 +3,14 @@ const hiddenBar =document.querySelector(".nav_box_hamburger");
 const hiddenBarContents = document.querySelector("#hamburger_menu");
 const c = document.querySelector("#close");
 
-function clickEvent(){
+function open(){
     hiddenBarContents.style.display = "block";  
 }
 
-function closedEvent(){
+function close(){
     hiddenBarContents.style.display = "none";  
 }
 
-function scrollevent() {
-    
-}
 
-hiddenBar.addEventListener("click", clickEvent);
-c.addEventListener("click", closedEvent);
-window.addEventListener("scroll", scrollevent);
+hiddenBar.addEventListener("click", open);
+c.addEventListener("click", close);
